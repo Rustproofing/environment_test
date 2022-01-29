c8190ecb4133253d52b3db8e20a16f87e1bdd9b9
Christopher He <christopher_he@tamu.edu>
2022-01-29 02:38:34 -0600

    update coverage

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,23 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+RSpec.describe 'Creating a book with author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Timothy'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Timothy')
+  end
+end
+RSpec.describe 'Creating a book with price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Price', with: '25.32'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('25.32')
+  end
+end
